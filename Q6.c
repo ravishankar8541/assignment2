a630c64e4300db62b70fb87c3f6ca19d91379bc6
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-11 16:17:51 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,10 @@
+//Write a program which takes a character as an input and displays its ASCII code.
+#include"stdio.h"
+int main()
+{
+    char ch;
+    printf("Enter a character : ");
+    scanf("%c",&ch);
+    printf("ASCII value of %c is %d",ch,ch);
+    return 0;
+}
