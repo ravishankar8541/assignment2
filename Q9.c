a369cbec9f32545d4129c8cd8467b82d096f1c9d
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-11 16:18:48 +0530

    Add files via upload

---
+++ Q9.c
@@ -0,0 +1,15 @@
+//Write a program to print size of an int, a float, a char and a double type variable
+#include"stdio.h"
+int main()
+{
+    int a;
+    char b;
+    float c;
+    double d;
+
+printf("%d",sizeof(a));
+printf("\n%d",sizeof(b));
+printf("\n%d",sizeof(c));
+printf("\n%d",sizeof(d));
+    return 0;
+}
