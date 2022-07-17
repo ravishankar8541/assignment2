eb9753e1835e0b240c1ecc54576b2f8747835df6
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-17 22:23:16 +0530

    Add files via upload

--- Q13.c
+++ Q13.c
@@ -2,17 +2,13 @@
 //one position towards the right.
 #include"stdio.h"
 int main()
-{int d1,d2,d3,n;
-int r1,r2,r3,sum;
-printf("Enter a number :");
-scanf("%d",&n);
-d1=((n/1)%10);
-r1=d1*100;
-d2=((n/10)%10);
-r2=d2*10;
-d3=((n/100)%10);
-r3=d3*1;
-sum=r1+r2+r3;
-printf("%d",sum);
+{
+    int n,temp,r;
+    printf("Enter three-digit number :");
+    scanf("%d",&n);
+    temp=n/10;
+    r=n%10;
+    n=r*100+temp;
+    printf("%d",n);
     return 0;
 }
